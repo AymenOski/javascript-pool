192d03fe6a0c7d77b7e3dda94f63a539fd32317a
AymenOski <aymenazizi610@gmail.com>
2025-06-18 18:37:15 +0100

    sweet

---
+++ Quest1/last-first-kiss.js
@@ -0,0 +1,5 @@
+const first = (str) => str[0];
+
+const last = (str) => str[str.length- 1];
+
+const kiss = (str) => [last(str),first(str)];
---
+++ Quest1/returns.js
@@ -0,0 +1,5 @@
+const id = (n) => n;
+
+const getLength = (n) => n.length;
+
+
