f6dc2689b106cdab7644c98f656a3a8687f32f1c
AymenOski <aymenazizi610@gmail.com>
2025-06-19 18:45:45 +0100

    mimicing Math.sign()

---
+++ Quest1/sign.js
@@ -0,0 +1,18 @@
+const sign = (num) => {
+    if (num === 0) {
+        return 0;
+    }
+    if (num > 0) {
+        return 1;
+    }else if (num < 0){
+        return -1;
+    }
+} 
+
+const sameSign = (a , b) => {
+    if ((a <= 0 && b <= 0) || (a >= 0 && b >= 0)){
+        return true;
+    }else {
+        return false;
+    }
+}
