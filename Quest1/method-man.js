0ca44fc7f5761b3b7cca50e23b2ae68a495da93e
AymenOski <aymenazizi610@gmail.com>
2025-06-19 18:30:25 +0100

    methods

---
+++ Quest1/method-man.js
@@ -0,0 +1,13 @@
+const words =(str) => str.split(' ');
+
+
+const sentence = (arr) => arr.join(' ');
+
+const yell = (str) => str.toUpperCase();
+
+const whisper = (str) => "*" + str.toLowerCase() + "*";
+
+const capitalize = (str) => str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+
+// console.log("hello".charAt(1).toUpperCase()); // E
+
