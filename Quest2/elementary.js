f2df687e5f67fa6dfc1b4a66459e419e71b3799a
AymenOski <aymenazizi610@gmail.com>
2025-06-20 18:27:56 +0100

    harder than i expected but sweet

--- Quest2/elementary.js
+++ Quest2/elementary.js
@@ -1,38 +1,69 @@
 
 
 // multiply that acts like the * operator, without using it.
-const multiply = (a,b) => {
-    let n = 0;
-    for (let i=0 ; i < b ; i++){
-        n += a
+const multiply = (a, b) => {
+    if (a < 0 && b < 0) {
+        return multiplyHelper(a, b);
+    } else if (a < 0 || b < 0) {        
+        return -multiplyHelper(a, b);
     }
+    return multiplyHelper(a, b);
+}
+
+const multiplyHelper = (a, b) => {
+    let n = 0;
+    // local a and b => we can change them witout effecting the origin
+    let [x, y] = checker(a, b)
+    
+    for (let i = 0; i < y; i++) {
+        n += x;
+    }
+    
     return n;
 }
 
+const checker = (a, b) => {
+    if (a < 0 && b < 0) {
+        return [-a, -b];
+    }
+    if (a < 0) {
+
+        a = -a;
+    }
+    if (b < 0) {
+
+        b = -b;
+    }    
+    return [a, b];
+}
 // divide that acts like the integer division operator /, without using it.
-const divide = (a,b) => {
+const divide = (a, b) => {
     let n = 0;
-    for (let i=0 ; i < a ; i++){
-        if (b * i > a ){
-            n = i-1 ;
+    let [x, y] = checker(a, b)
+    for (let i = 0; i < x; i++) {
+        if (multiply(y, i) > x) {
+            n = i - 1;
             break;
         }
+    }
+    if (a < 0 && b < 0) {
+        return n;
+    } else if (a < 0 || b < 0) {
+        return -n;
     }
     return n;
 }
 
 // modulo that acts like the % operator, without using it.
-const modulo = (a,b) => {
-    let s = divide(a,b);
-    if (a-(s*b) !==0 ){
-        return a-(s*b);
-    }else {
+const modulo = (a, b) => {
+    let s = divide(a, b);
+    if (a - (multiply(s, b)) !== 0) {
+        return a - (multiply(s, b));
+    } else {
         return 0;
     }
 }
 
-console.log(multiply(11,7));
-console.log(divide(24,2));
-console.log(modulo(25,2));
-
-
+// console.log(multiply(-123, 22));
+// console.log(divide(123, -22));
+// console.log(modulo(17, 2));
