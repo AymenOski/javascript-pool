1a47f92aa5e97039ca3ca63a0ae1980f0cbc9d81
AymenOski <aymenazizi610@gmail.com>
2025-06-23 09:28:06 +0100

    adding exercice instruction

--- Quest2/repeat.js
+++ Quest2/repeat.js
@@ -1,3 +1,12 @@
+/*
+
+Instructions
+Create a function named repeat that takes a string and a number as arguments,
+and returns the string repeated as many times as the number describes.
+It should function like String.repeat(), but of course you must make your own.
+
+*/
+
 const repeat = (str , num ) => {
     var s = "";
     for (let i = 0; i < num; i++) {
