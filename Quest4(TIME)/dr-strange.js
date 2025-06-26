ca94d54937e2ac5f3ef256719cd0973f3d995e77
AymenOski <aymenazizi610@gmail.com>
2025-06-26 13:20:04 +0100

    time travel completed

--- Quest4(TIME)/dr-strange.js
+++ Quest4(TIME)/dr-strange.js
@@ -11,28 +11,20 @@ function addWeek(date){
   return days[timeInDays % 14];
 }
 
-function timeTravel(obj){
-  console.log();
+function timeTravel({date, hour ,minute ,second}){
   
-  // console.log(obj.date[Symbol.toPrimitive]("string"));
-  
-  format = format.replace("%s", "Aymen").replace("%d", 21);
-
-  return 
+  date.setHours(hour);
+  date.setMinutes(minute);
+  date.setSeconds(second);
+  return date;
 }
 
-timeTravel({
-  date: new Date('2020-05-29 23:25:22'),
-  hour: 21,
-  minute: 22,
-  second: 22,
-}).toString()
-
-console.log(addWeek(new Date('0001-01-01')));
-console.log(addWeek(new Date('0001-01-02')));
-console.log(addWeek(new Date('0001-01-07')));
-console.log(addWeek(new Date('0001-01-08')));
-console.log(addWeek(new Date('0001-01-09')));
-
-
+// console.log(
+//   timeTravel({
+//     date: new Date('2020-05-29 23:25:22'),
+//     hour: 21,
+//     minute: 22,
+//     second: 22,
+//   }).toString()
+// );
 
