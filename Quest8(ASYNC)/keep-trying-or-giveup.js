f9e67877355fd9463b440515ce1c0edaff4fa250
AymenOski <aymenazizi610@gmail.com>
2025-07-07 04:30:18 +0100

    nice training for async-await

--- Quest8(ASYNC)/keep-trying-or-giveup.js
+++ Quest8(ASYNC)/keep-trying-or-giveup.js
@@ -1,4 +1,27 @@
-const retry = () => {
+const retry = (maxCount, callback) => {
+    let count = 0;
+    return async function func(...args){
+        try {
+            return await callback(...args)
+        } catch (error) {
+            if (count === maxCount){
+                throw error;
+            }
+            count++;
+            return await func(...args)
+        }
+    };
+};
 
+const timeout = (delay ,callback) => {
+    let x = new Promise((_, reject) =>
+      setTimeout(() => reject(new Error("timeout")), delay)
+    )
+    return async function (...args) {
+        try {
+            return await Promise.race([callback(...args),x])
+        } catch (error) {
+            throw error
+        }
+    }
 }
-
