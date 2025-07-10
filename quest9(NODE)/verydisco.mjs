ce3e41a2719349bd31e8bf9bcf0c799d113f5aa5
AymenOski <aymenazizi610@gmail.com>
2025-07-10 15:09:09 +0100

    first .mjs

--- quest9(NODE)/verydisco.mjs
+++ quest9(NODE)/verydisco.mjs
@@ -1,37 +1,33 @@
 const args = process.argv.slice(2);
-const disco = args.length !== 0 ?  args.slice(0, 1).join('') : '';
+const disco = args.length !== 0 ? args.slice(0, 1).join('') : '';
 
-const verydisco = () => {
-    let firstHalf = '';
-    let secendHalf = '';    
-    let s ='';
-    if (disco === ''){
-        return '';
-    }
-    if (disco.includes(' ')) {        
-        var newDisco = disco.split(' ')
-        newDisco.forEach(element => {
-            if (element.length % 2 === 0) {
-                 firstHalf = element.slice(0, (element.length / 2));
-                 secendHalf = element.slice((element.length / 2))
-                 s += secendHalf + firstHalf + ' ';
-            } else {
-                 firstHalf = element.slice(0, (element.length / 2) + 1);
-                 secendHalf = element.slice((element.length / 2) + 1);
-                 s += secendHalf + firstHalf + ' ';
-            }
-        });
-        return s;
-    }
-        if (disco.length % 2 === 0) {
-             firstHalf = disco.slice(0, (disco.length / 2));
-             secendHalf = disco.slice((disco.length / 2))
-        } else {
-             firstHalf = disco.slice(0, (disco.length / 2) + 1);
-             secendHalf = disco.slice((disco.length / 2) + 1)
-        }
-
-    return secendHalf + firstHalf;
+let firstHalf = '';
+let secendHalf = '';
+let s = [];
+if (disco === '') {
+    process.exit(0);
 }
-// console.log(verydisco());
+if (disco.includes(' ')) {
+    var newDisco = disco.split(' ')
+    newDisco.forEach(element => {
+        if (element.length % 2 === 0) {
+            firstHalf = element.slice(0, Math.ceil((element.length / 2) - 1));
+            secendHalf = element.slice(Math.ceil((element.length / 2) - 1))
+        } else {
+            firstHalf = element.slice(0, Math.ceil((element.length / 2)));
+            secendHalf = element.slice((Math.ceil(element.length / 2)));
+        }
+        s.push(secendHalf + firstHalf);
+    });
+    console.log(s.join(' '));
+    process.exit(0)
+}
+if (disco.length % 2 === 0) {
+    firstHalf = disco.slice(0, (Math.ceil(disco.length / 2)));
+    secendHalf = disco.slice(Math.ceil((disco.length / 2)))
+} else {
+    firstHalf = disco.slice(0, (Math.ceil(disco.length / 2) - 1));
+    secendHalf = disco.slice(Math.ceil((disco.length / 2) - 1))
+}
+console.log(secendHalf + firstHalf);
 
